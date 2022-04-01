62338db1b61f6574fba2d4095dc1d8a9e18a781b
Khai Tran <khaiqtran13@gmail.com>
2022-04-01 16:59:20 -0400

    connection attempt WIP

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,7 +1,13 @@
 import express, { Application, Request, Response } from "express";
 
+const Sequelize = require("sequelize");
 const app: Application = express();
 const port = 8000;
+
+const database =
+    "postgres://tsvxqwcbioynvl:bebb014e18df1bd9c0f6bd949264f9c0e3b0a978aa3849c3028fee5ab8fa2279@ec2-3-229-252-6.compute-1.amazonaws.com:5432/dabna7dvuqs2bq";
+
+const sequelize = new Sequelize(database);
 
 // Body parsing Middleware
 app.use(express.json());
