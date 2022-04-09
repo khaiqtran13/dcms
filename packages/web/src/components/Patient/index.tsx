4aec6780ccc657b3c3068cefa258965abc20a9b8
Chetan Mandur <ChetanMandur45@gmail.com>
2022-04-09 00:50:39 -0400

    finished patient routing

--- packages/web/src/components/Patient/index.tsx
+++ packages/web/src/components/Patient/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import MedHistory from './MedHistory';
+import PatientHome from './PatientHome';
+import ScheduleApps from './ScheduleApps';
+import UpcomingApps from './UpcomingApps';
 type Props = {}
 
 const PatientMain = (props: Props) => {
   return (
-    <div>You are a patient</div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<PatientHome/>} ></Route>
+          <Route path="history" element={<MedHistory/>} ></Route>
+          <Route path="schedule" element={<ScheduleApps/>} ></Route>
+          <Route path="upcoming" element={<UpcomingApps/>} ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
