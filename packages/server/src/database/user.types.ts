38a90b4d48d0b48a9fa2d57e46ddccd418cbcb3d
Khai Tran <khaiqtran13@gmail.com>
2022-04-11 18:11:42 -0400

    add patient

--- packages/server/src/database/user.types.ts
+++ packages/server/src/database/user.types.ts
@@ -10,3 +10,20 @@ export interface IUser {
     role: string;
     ssn: number;
 }
+
+export interface IEmployee extends IUser {
+    employee_id: number; // PK
+    record_id: string; // FK
+    employee_type: string;
+    salary: number;
+}
+
+export interface IPatient extends IUser {
+    patient_id?: number; // PK
+    gender: string;
+    insurance: string;
+    email_address: string;
+    DOB: Date; // or string?
+    payment_id?: string; // FK
+    record_id?: string; // FK
+}
