bb780e3432e6b27e851cff7bb53fef77b0016e3e
Khai Tran <khaiqtran13@gmail.com>
2022-04-11 20:02:19 -0400

    straight to master

--- packages/server/src/database/gen.types.ts
+++ packages/server/src/database/gen.types.ts
@@ -5,3 +5,16 @@ export interface IReview {
     cleanliness: number;
     value: number;
 }
+
+export interface IAppointment {
+    fee_id?: string;
+    patient_id?: string;
+    dentist_id?: number;
+    user_id: number;
+    startDate: Date;
+    endDate: Date;
+    status: string;
+    cancelDate: Date;
+    appointment_type: string;
+    appointment_id: number; // idk
+}
