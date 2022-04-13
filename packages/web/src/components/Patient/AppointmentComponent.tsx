2bc7c525d2034fbb7d4a7b3e0458d43674c251d8
Khai Tran <khaiqtran13@gmail.com>
2022-04-12 23:54:49 -0400

    statsu change

--- packages/web/src/components/Patient/AppointmentComponent.tsx
+++ packages/web/src/components/Patient/AppointmentComponent.tsx
@@ -63,7 +63,7 @@ export const AppointmentComponent = (props: Props) => {
             const appointment: IAppointment = {
                 user_id: context?.user?.user_id,
                 start_date: startValue,
-                status: "pending",
+                status: "incomplete",
                 appointment_type: procedure,
                 appointment_id: 0,
                 dentist_id: selectedDentistID,
