b4add6d7138099d6e086919b6b8287f46cc1dde9
Chetan Mandur <ChetanMandur45@gmail.com>
2022-04-14 16:12:22 -0400

    Added API call that gets whole patient object from patient ID

--- packages/server/src/api/api.ts
+++ packages/server/src/api/api.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import {
-  addPatient,
-  editPatient,
-  setAppointment,
-  getAppointmentByPatientId,
-  getAppointments,
-  getDentists,
-  getLogin,
-  getPatientRecordById,
-  getPatients,
-  getRecords,
-  getUserById,
-  getUsers,
-  getUserByRole,
+    addPatient,
+    editPatient,
+    setAppointment,
+    getAppointmentByPatientId,
+    getAppointments,
+    getDentists,
+    getLogin,
+    getPatientRecordById,
+    getPatients,
+    getRecords,
+    getUserById,
+    getUsers,
+    getUserByRole,
+    getPatientObject,
 } from "../endpoints/endpoints";
 
 const apiRouter = express.Router();
@@ -32,13 +33,14 @@ apiRouter.get("/user/dentists", getDentists);
 apiRouter.post("/user/add", addPatient);
 apiRouter.post("/user/edit/:user_id", editPatient);
 apiRouter.get("/user/:user_id", getUserById);
-apiRouter.get("/user/role/:role", getUserByRole)
+apiRouter.get("/user/role/:role", getUserByRole);
 
 // login
 apiRouter.post("/login", getLogin);
 
 //patients
 apiRouter.get("/patients", getPatients);
+apiRouter.get("/patients/object/:patient_id", getPatientObject);
 
 //records
 apiRouter.get("/records", getRecords);
