07cac4654b1fa6780e80d6c9e5ff411847e1a55f
Khai Tran <khaiqtran13@gmail.com>
2022-04-14 16:10:14 -0400

    wtf

--- packages/web/src/components/Dentist/index.tsx
+++ packages/web/src/components/Dentist/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import ViewPatientInfoComponent from "./ViewPatientInfoComponent";
 
 type Props = {};
 
 const DentistMain = (props: Props) => {
-    return <div>ok idk what other pages dentist might need, please help</div>;
+    return (
+        <div className="flex flex-col mx-auto place-content-center space-y-4 h-screen w-96">
+            <ViewPatientInfoComponent />
+        </div>
+    );
 };
 
 export default DentistMain;
